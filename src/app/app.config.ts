2dd5e27e6ff5733fd26ac481b588505c267b3ce9
Sandeep <sandeepkbh47@gmail.com>
2024-07-14 23:36:34 +0530

    ngrx lazy loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,28 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { StoreModule } from '@ngrx/store';
+import { createFeature, StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/state/counter.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { postReducer } from './post/state/post.reducer';
 import { CounterInterface } from './counter/state/counter-interface';
 import { PostsInterface } from './post/state/post-state';
+import { provideRouterStore } from '@ngrx/router-store';
 export interface AppState {
   counter: CounterInterface;
   posts: PostsInterface;
 }
+
+export const counterFeature = createFeature({
+  name: 'counter',
+  reducer: counterReducer,
+});
+
+export const postFeature = createFeature({
+  name: 'posts',
+  reducer: postReducer,
+});
+
 export const storeConfig = {
   counter: counterReducer,
   posts: postReducer,
@@ -19,8 +31,9 @@ export const storeConfig = {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideRouterStore(),
     importProvidersFrom(
-      StoreModule.forRoot(storeConfig),
+      StoreModule.forRoot({}),
       StoreDevtoolsModule.instrument({
         maxAge: 25,
         logOnly: false,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 import { CounterComponent } from './counter/counter/counter.component';
 import { PostComponent } from './post/post/post.component';
 import { AddPostComponent } from './post/add-post/add-post.component';
+import { StoreModule } from '@ngrx/store';
+import { counterFeature, postFeature } from './app.config';
+import { importProvidersFrom } from '@angular/core';
+import { postReducer } from './post/state/post.reducer';
+import { counterReducer } from './counter/state/counter.reducer';
 
 export const routes: Routes = [
   {
@@ -10,10 +15,14 @@ export const routes: Routes = [
       import('./counter/counter/counter.component').then(
         (c) => c.CounterComponent
       ),
+    providers: [
+      importProvidersFrom(StoreModule.forFeature('counter', counterReducer)),
+    ],
   },
   {
     path: 'posts',
-    component: PostComponent,
+    loadComponent: () =>
+      import('./post/post/post.component').then((c) => c.PostComponent),
     children: [
       {
         path: 'add',
@@ -23,6 +32,9 @@ export const routes: Routes = [
         path: 'edit/:id',
         component: AddPostComponent,
       },
+    ],
+    providers: [
+      importProvidersFrom(StoreModule.forFeature('posts', postReducer)),
     ],
   },
 ];
